19604e67ff257c68eaa8d1b29dc509785a355a26
Michal S <Michal Stetina>
2025-05-04 17:49:08 +0200

    Fix - download path changed to src folder

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -4,6 +4,6 @@
 echo "Starting download..."
 
 cd src
-s5cmd --no-sign-request run scripts/Data\ fetcher/download_5percent.s5cmd
+s5cmd --no-sign-request run ../scripts/Data\ fetcher/download_5percent.s5cmd
 
 echo "Download complete. Data saved to data/ directory."
