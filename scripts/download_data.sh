62efc47ad5960122e5765f6a9edcefd28df25513
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2025-05-04 17:15:36 +0200

    Fix - change dir to src for data download

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -2,6 +2,8 @@
 
 # Run the download commands
 echo "Starting download..."
+
+cd src
 s5cmd --no-sign-request run scripts/Data\ fetcher/download_5percent.s5cmd
 
 echo "Download complete. Data saved to data/ directory."
